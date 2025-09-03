5f0598f8618a78234b73a90f1b16487f6c78c3d5
felipeFerreiraffl <felipado2x0@gmail.com>
2025-09-03 20:21:24 -0300

    feat: added new setup for jest

---
+++ backend/jest.config.js
@@ -0,0 +1,20 @@
+/** @type(import("jest").config) */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src", "<rootDir>/tests"],
+  testMatch: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts"],
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+  },
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/**/*.d.ts",
+    "!src/server.ts", // Exclui o arquivo do servidor
+  ],
+  setupFilesAfterEnv: ["<rootDir>/tests/setup.ts"],
+  moduleNameMapping: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  testTimeout: 30000,
+};
---
+++ backend/tests/setup.ts
@@ -0,0 +1,36 @@
+/* ---------- 🧪 Setup do Jest ---------- */
+
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { MongoMemoryServer } from "mongodb-memory-server";
+
+dotenv.config();
+
+let mongoServer: MongoMemoryServer;
+
+beforeAll(async () => {
+  // Cria um servidor em memória para testes
+  mongoServer = await MongoMemoryServer.create();
+  const mongouri = mongoServer.getUri();
+
+  // Conexão ao servidor em memória
+  await mongoose.connect(mongouri);
+});
+
+// Cada teste
+afterEach(async () => {
+  // Limpa as collections após cada teste
+  const collections = mongoose.connection.collections;
+  for (const key in collections) {
+    await collections[key].deleteMany({});
+  }
+});
+
+// Fechamento dos testes
+afterAll(async () => {
+  await mongoose.connection.close();
+  await mongoServer.stop();
+});
+
+// Configuração global
+jest.setTimeout(30000);
