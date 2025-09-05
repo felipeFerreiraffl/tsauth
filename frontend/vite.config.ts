f737db322b57b6eef986aaf7db1583c618805bae
felipeFerreiraffl <felipado2x0@gmail.com>
2025-09-05 20:24:48 -0300

    feat: created frontend folders and its initial structure

---
+++ frontend/vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+
+  // Configurações de desenvolvimento
+  server: {
+    port: 8080,
+    proxy: {
+      "/api": {
+        target: "/http:localhost:8080",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+
+  // Otimização de build
+  build: {
+    target: "esnext",
+    minify: "esbuild",
+    sourcemap: true,
+  },
+
+  // Configuração de TypeScript
+  esbuild: {
+    logOverride: { "this-is-undefined-in-esm": "silent" },
+  },
+});
