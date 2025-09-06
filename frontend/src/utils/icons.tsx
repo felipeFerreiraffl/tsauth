7cdead86b14a37c4fbb5185802b47ee7d73603fa
felipeFerreiraffl <felipado2x0@gmail.com>
2025-09-06 14:05:31 -0300

    feat: added consts files for images and icons

---
+++ frontend/src/utils/icons.tsx
@@ -0,0 +1,23 @@
+/* ---------- Ícones ---------- */
+
+import IcnEmail from "../assets/svgs/icons/email.svg?react";
+import IcnGithub from "../assets/svgs/icons/email.svg?react";
+import IcnInstagram from "../assets/svgs/icons/email.svg?react";
+import IcnInvisible from "../assets/svgs/icons/email.svg?react";
+import IcnLinkedin from "../assets/svgs/icons/email.svg?react";
+import IcnPadlock from "../assets/svgs/icons/email.svg?react";
+import IcnPhoneCall from "../assets/svgs/icons/email.svg?react";
+import IcnUser from "../assets/svgs/icons/email.svg?react";
+
+const icons = {
+  email: <IcnEmail />,
+  github: <IcnGithub />,
+  instagram: <IcnInstagram />,
+  invisible: <IcnInvisible />,
+  linkedin: <IcnLinkedin />,
+  padlock: <IcnPadlock />,
+  phoneCall: <IcnPhoneCall />,
+  user: <IcnUser />,
+};
+
+export default icons;
---
+++ frontend/src/utils/images.ts
@@ -0,0 +1,13 @@
+/* ---------- Imagens ---------- */
+
+import dev from "../assets/imgs/dev-illustration.jpg";
+import home from "../assets/imgs/home-illustration.jpg";
+import logo from "../assets/logos/logo.svg";
+
+const images = {
+  dev: dev,
+  home: home,
+  logo: logo,
+};
+
+export default images;
