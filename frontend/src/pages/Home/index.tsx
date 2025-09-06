f8ca071906d83bced807faaafbc6abd16b245ee7
felipeFerreiraffl <felipado2x0@gmail.com>
2025-09-06 14:15:32 -0300

    chore: added fill to logo

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,10 @@
+import images from "../../utils/images";
 import styles from "./styles.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <img src="" alt="" />
+      <img src={images.logo} alt="TSAuth logo" />
     </div>
   );
 }
