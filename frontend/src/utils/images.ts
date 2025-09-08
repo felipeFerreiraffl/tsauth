6e8ab7f4a7c41e848299712fbb41ebbe9ea2daa5
felipeFerreiraffl <felipado2x0@gmail.com>
2025-09-08 15:57:02 -0300

    chore: added new images variables on utils/images file

--- frontend/src/utils/images.ts
+++ frontend/src/utils/images.ts
@@ -3,11 +3,17 @@
 import dev from "../assets/imgs/programmer-illustration.png";
 import home from "../assets/imgs/home-illustration.png";
 import logo from "../assets/logos/logo.svg";
+import github from "../assets/svgs/icons/github.svg";
+import instagram from "../assets/svgs/icons/instagram.svg";
+import linkedin from "../assets/svgs/icons/linkedin.svg";
 
 const images = {
-  programmer: dev,
-  home: home,
-  logo: logo,
+  dev,
+  home,
+  logo,
+  github,
+  instagram,
+  linkedin,
 };
 
 export default images;
